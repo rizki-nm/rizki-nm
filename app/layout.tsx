857d8fc87b465a7e02b40441034b7fe1233bf634
Rizki Nuzul Muzaki <rizki.nm01@gmail.com>
2023-08-20 13:39:12 +0700

    FEAT: add title staging

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,7 @@ import { Toaster } from "react-hot-toast";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Rizki | Personal Portfolio",
+  title: "Staging | Rizki | Personal Portfolio",
   description: "Rizki is a final-year student with 1 years of intern experience.",
 };
 
